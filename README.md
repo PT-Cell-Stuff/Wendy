a0ee76d8de3154e7a1f8a5878ad93ff9cd4194c3
Aniruddh Radhakrishnan <10290780-loggerheads-with-binary@users.noreply.gitlab.com>
2022-08-20 22:49:10 +0530

    Second commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Wendy
+Wendy is a FUSE(FileSystem in UserSpacE)  virtual file system(VFS) based on Telegram. It uses the following packages:
+
+1. Telethon for Communication with Telegram Servers through MTProto Protocol 
+2. Sqlite for maintaining the file/directory database 
+3. Jihyocrypt for Encryption and Decryption of files 
+4. Flat directories for maintaining cache 
+
+# Current Functionalities: 
+
+1. Concurrent Uploads and Downloads 
+2. Multiple encryption channels 
+3. Multiple peers/entities to send files to 
+
+
+# To develop 
+
+1. File sharing 
+2. Real time decryption 
+3. Cloud Compression 
+4. Video Content Delivery Service 
+
--- concurrency.py
+++ concurrency.py
@@ -1,5 +1,12 @@
 import multiprocessing 
 import logging 
+import telethon.ext.futures ##Patched file 
+from stex.tools import add_fileupload 
+from stex.tools import add_filedownload 
+from . import config as cfg 
+
+add_fileupload.semaphore = multiprocessing.Semaphore(n = cfg.MAX_UPLOAD_LIMIT )
+add_filedownload.semaphore = multiprocessing.Semaphore(n = cfg.MAX_DOWNLOAD_LIMIT )
 
 logger = logging.getLogger('wendy.concurrency')
 logger.setLevel(logging.DEBUG)
@@ -19,4 +26,11 @@ def set_semaphores(config):
     EncDec = multiprocessing.Semaphore(config.encdec)
     Upload = multiprocessing.Semaphore(config.upload)
     Download = multiprocessing.Semaphore(config.download)
+
+def file_download(*args):
     
+    return telethon.ext.futures.future(add_filedownload.semaphore.manage(*args))    
+    
+def file_upload(*args):
+    
+    return telethon.ext.futures.future(add_fileupload.semaphore.manage(*args))
